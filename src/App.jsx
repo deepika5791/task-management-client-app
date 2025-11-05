7cb991c37e4747d724ddda389d141606b9edfa91
deepika5791 <deepikatripathi0810@gmail.com>
2025-11-05 13:40:24 +0530

    added color of text

--- src/App.jsx
+++ src/App.jsx
@@ -10,33 +10,10 @@ import "./App.css";
 const App = () => {
   const location = useLocation();
   const hideNavbar = ["/login", "/signup"].includes(location.pathname);
-  const [darkMode, setDarkMode] = useState(() => {
-    return localStorage.getItem("darkMode") === "true";
-  });
-
-  useEffect(() => {
-    localStorage.setItem("darkMode", darkMode);
-    if (darkMode) document.documentElement.classList.add("dark-mode");
-    else document.documentElement.classList.remove("dark-mode");
-  }, [darkMode]);
 
   return (
     <div className="app-container">
       {!hideNavbar && <Navbar />}
-      <button
-        className="mode-toggler-btn"
-        onClick={() => setDarkMode((prev) => !prev)}
-      >
-        {darkMode ? (
-          <>
-            <i class="bi bi-moon-fill">Light Mode</i>
-          </>
-        ) : (
-          <>
-            <i class="bi bi-brightness-low-fill">Dark Mode</i>
-          </>
-        )}
-      </button>
 
       <div className={!hideNavbar ? "content-wrapper" : ""}>
         <main className="main-container">
