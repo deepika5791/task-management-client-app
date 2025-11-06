52de74cb6ecb35ae5cdebd392690a48d3122fc3d
deepika5791 <deepikatripathi0810@gmail.com>
2025-11-06 18:00:04 +0530

    added FrontPage Component

---
+++ src/pages/frontPage/FrontPage.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./FrontPage.css";
+
+const FrontPage = () => {
+  const navigate = useNavigate();
+
+  const handleGetStarted = () => {
+    navigate("/signup");
+  };
+
+  return (
+    <div className="home-hero">
+      <div className="hero-left">
+        <h1>Organize your tasks effortlessly</h1>
+        <p className="subtitle">
+          Manage tasks, track progress, and boost your productivity — all in one
+          place.
+        </p>
+
+        <button className="get-started" onClick={handleGetStarted}>
+          Get Started
+        </button>
+      </div>
+
+      <div className="hero-right">
+        <img
+          src={require("../../assets/task-list-svg")}
+          alt="Task Management Illustration"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default FrontPage;
