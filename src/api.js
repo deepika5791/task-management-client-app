9d3e9242e038146d9209db3f24423d5e16c5c6c6
deepika7891 <deepikatripathi0810@gmail.com>
2025-11-24 18:06:46 +0530

    added backend link

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://task-management-ylnb.onrender.com/api",
+  baseURL: "https://task-management-ud08.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
