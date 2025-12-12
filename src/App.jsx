7428948337cf2694f37c80173d615cb0cdfb532f
deepika7891 <deepikatripathi0810@gmail.com>
2025-12-12 20:36:13 +0530

    added filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,72 @@
 
+// import React, { useContext } from "react";
+// import { Routes, Route, useLocation } from "react-router-dom";
+// import Home from "./pages/Home/Home";
+// import BoardPage from "./pages/BoardPage/BoardPage";
+// import Signup from "./pages/SignUp/Signup";
+// import Navbar from "./components/Navbar/Navbar";
+// import "./App.css";
+// import FrontPage from "./pages/frontPage/FrontPage";
+// import Login from "./pages/LoginPage/Login";
+// import Footer from "./components/footer/Footer";
+// import { AuthContext } from "./context/AuthProvider";
+// import GlobalLoginPage from "./components/globalLoginPage/GlobalLoginPage";
+
+// const App = () => {
+//   const location = useLocation();
+//   const hideNavbar = ["/login", "/signup"].includes(location.pathname);
+//   const { user } = useContext(AuthContext);
+
+//   return (
+//     <div className="app-container">
+//       <Navbar />
+
+//       <div className={!hideNavbar ? "content-wrapper" : ""}>
+//         <main className="main-container">
+//           <Routes>
+
+//             <Route path="/" element={<FrontPage />} />
+//             <Route path="/signup" element={<Signup />} />
+//             <Route path="/login" element={<Login />} />
+//             <Route path="/board/:id" element={<BoardPage />} />
+
+//             <Route
+//               path="/home"
+//               element={
+//                 user ? (
+//                   <div className="content-wrap">
+//                     <Home />
+//                   </div>
+//                 ) : (
+//                   <>
+//                     <div className="blur-background"></div>
+//                     <GlobalLoginPage />
+//                   </>
+//                 )
+//               }
+//             />
+
+//             <Route
+//               path="/globalLoginPage"
+//               element={
+//                 <>
+//                   <div className="blur-background"></div>
+//                   <GlobalLoginPage />
+//                 </>
+//               }
+//             />
+
+//           </Routes>
+//         </main>
+//       </div>
+
+//       <Footer />
+//     </div>
+//   );
+// };
+
+// export default App;
+
 import React, { useContext } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home/Home";
@@ -19,44 +87,40 @@ const App = () => {
 
   return (
     <div className="app-container">
+      {/* Navbar stays always on top and is NOT blurred */}
       <Navbar />
 
       <div className={!hideNavbar ? "content-wrapper" : ""}>
         <main className="main-container">
-          <Routes>
-
-            <Route path="/" element={<FrontPage />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/board/:id" element={<BoardPage />} />
-
-            <Route
-              path="/home"
-              element={
-                user ? (
+          {user ? (
+            <Routes>
+              <Route path="/" element={<FrontPage />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/board/:id" element={<BoardPage />} />
+              <Route
+                path="/home"
+                element={
                   <div className="content-wrap">
                     <Home />
                   </div>
-                ) : (
-                  <>
-                    <div className="blur-background"></div>
-                    <GlobalLoginPage />
-                  </>
-                )
-              }
-            />
-
-            <Route
-              path="/globalLoginPage"
-              element={
-                <>
-                  <div className="blur-background"></div>
-                  <GlobalLoginPage />
-                </>
-              }
-            />
-
-          </Routes>
+                }
+              />
+            </Routes>
+          ) : (
+            <>
+              {/* Blur only the background content, Navbar remains clear */}
+              <div className="blur-background"></div>
+              <Routes>
+                <Route path="/" element={<FrontPage />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/board/:id" element={<BoardPage />} />
+                <Route path="/home" element={<GlobalLoginPage />} />
+                <Route path="/globalLoginPage" element={<GlobalLoginPage />} />
+              </Routes>
+            </>
+          )}
         </main>
       </div>
 
@@ -66,4 +130,3 @@ const App = () => {
 };
 
 export default App;
-
