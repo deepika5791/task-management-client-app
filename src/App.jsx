ef29891f7506d03f79dae2218f80037cd058063c
deepika7891 <deepikatripathi0810@gmail.com>
2025-12-12 13:17:36 +0530

    added color

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
     <div className="app-container">
       <Navbar />
       <div className={!hideNavbar ? "content-wrapper" : ""}>
-          <div className={user ? "content-wrapper" : "wrapper"}>
+          <div className={user ?  "wrapper" : "content-wrapper"}>
         <main className="main-container">
           <Routes>
             <Route
