e00b1e7e3cebd9eeacbe3ebef966c7a2209db11c
deepika7891 <deepikatripathi0810@gmail.com>
2025-12-12 19:12:47 +0530

    added new classname

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,17 @@ import Login from "./pages/LoginPage/Login";
 import Footer from "./components/footer/Footer";
 import { AuthContext } from "./context/AuthProvider";
 import GlobalLoginPage from "./components/globalLoginPage/GlobalLoginPage";
+import { Outlet } from "react-router-dom";
 const App = () => {
   const location = useLocation();
   const hideNavbar = ["/login", "/signup"].includes(location.pathname);
   const { user } = useContext(AuthContext);
 
-  
+  const UserLayout = ({ user }) => (
+    <div className={user ? "content-wrap" : "wrapper"}>
+      <Outlet />
+    </div>
+  );
 
   return (
     <div className="app-container">
@@ -26,12 +31,21 @@ const App = () => {
             <Route path="/" element={<FrontPage />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/login" element={<Login />} />
-            <Route
+            <Route path="/board/:id" element={<BoardPage />} />
+            <Routes>
+              <Route element={<UserLayout user={user} />}>
+                {/* <Route
               path="/home"
               element={<div className={user ? "content-wrap" : "wrapper"}>{user ? <Home /> : <GlobalLoginPage />}</div>}
             />
-            <Route path="/board/:id" element={<BoardPage />} />
-            <Route path="/globalLoginPage" element={<div className={user ? "content-wrap" : "wrapper"}><GlobalLoginPage /></div> } />
+            <Route path="/globalLoginPage" element={<div className={user ? "content-wrap" : "wrapper"}><GlobalLoginPage /></div> } /> */}
+                <Route
+                  path="/home"
+                  element={user ? <Home /> : <GlobalLoginPage />}
+                />
+                <Route path="/globalLoginPage" element={<GlobalLoginPage />} />
+              </Route>
+            </Routes>
           </Routes>
         </main>
       </div>
