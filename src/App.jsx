4f08129915bdb00d0d4d18e7e247e0d013994b93
deepika7891 <deepikatripathi0810@gmail.com>
2025-12-12 20:21:42 +0530

    added filter

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,75 @@
 // };
 
 // export default App;
+// import React, { useContext } from "react";
+// import { Routes, Route, useLocation } from "react-router-dom";
+// import Home from "./pages/Home/Home";
+// import BoardPage from "./pages/BoardPage/BoardPage";
+// import Signup from "./pages/SignUp/Signup";
+// import Navbar from "./components/Navbar/Navbar";
+// import "./App.css";
+// import FrontPage from "./pages/frontPage/FrontPage";
+// import Login from "./pages/LoginPage/Login";
+// import Footer from "./components/footer/Footer";
+// import { AuthContext } from "./context/AuthProvider";
+// import GlobalLoginPage from "./components/globalLoginPage/GlobalLoginPage";
+
+// const App = () => {
+//   const location = useLocation();
+//   const hideNavbar = ["/login", "/signup"].includes(location.pathname);
+//   const { user } = useContext(AuthContext);
+
+//   return (
+//     <div className="app-container">
+//       <Navbar />
+
+//       <div className={!hideNavbar ? "content-wrapper" : ""}>
+//         <main className="main-container">
+//           <Routes>
+
+//             <Route path="/" element={<FrontPage />} />
+//             <Route path="/signup" element={<Signup />} />
+//             <Route path="/login" element={<Login />} />
+//             <Route path="/board/:id" element={<BoardPage />} />
+
+//             {/* ------ HOME PAGE ------ */}
+//             <Route
+//               path="/home"
+//               element={
+//                 user ? (
+//                   <div className="content-wrap">
+//                     <Home />
+//                   </div>
+//                 ) : (
+//                   <>
+//                     <div className="blur-background"></div>
+//                     <GlobalLoginPage />
+//                   </>
+//                 )
+//               }
+//             />
+
+//             {/* ------ GLOBAL LOGIN PAGE (always blur background) ------ */}
+//             <Route
+//               path="/globalLoginPage"
+//               element={
+//                 <>
+//                   <div className="blur-background"></div>
+//                   <GlobalLoginPage />
+//                 </>
+//               }
+//             />
+
+//           </Routes>
+//         </main>
+//       </div>
+
+//       <Footer />
+//     </div>
+//   );
+// };
+
+// export default App;
 import React, { useContext } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home/Home";
@@ -63,25 +132,28 @@ const App = () => {
     <div className="app-container">
       <Navbar />
 
-      <div className={!hideNavbar ? "content-wrapper" : ""}>
+      {/* WRAP CONTENT SO ONLY THIS AREA CAN BLUR */}
+      <div
+        id="main-content"
+        className={!hideNavbar ? "content-wrapper" : ""}
+        style={{ position: "relative" }}
+      >
         <main className="main-container">
           <Routes>
-
             <Route path="/" element={<FrontPage />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/login" element={<Login />} />
             <Route path="/board/:id" element={<BoardPage />} />
 
-            {/* ------ HOME PAGE ------ */}
+            {/* ---------------- HOME ROUTE ---------------- */}
             <Route
               path="/home"
               element={
                 user ? (
-                  <div className="content-wrap">
-                    <Home />
-                  </div>
+                  <Home />
                 ) : (
                   <>
+                    {/* BLUR ONLY MAIN-CONTENT */}
                     <div className="blur-background"></div>
                     <GlobalLoginPage />
                   </>
@@ -89,7 +161,7 @@ const App = () => {
               }
             />
 
-            {/* ------ GLOBAL LOGIN PAGE (always blur background) ------ */}
+            {/* ---------------- GLOBAL LOGIN ROUTE ---------------- */}
             <Route
               path="/globalLoginPage"
               element={
@@ -99,7 +171,6 @@ const App = () => {
                 </>
               }
             />
-
           </Routes>
         </main>
       </div>
