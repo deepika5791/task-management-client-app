c30467ea7985e3f0ea18944fe962b9a9fbb0ab72
deepika7891 <deepikatripathi0810@gmail.com>
2025-12-16 18:33:36 +0530

    removed the div

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -163,7 +163,7 @@ const Home = () => {
       </div>
 
       <form className="new-board-form" onSubmit={createBoard}>
-        <div style={{ flex: 1 }}>
+       
           <input
             type="text"
             placeholder="Create a new board (e.g. Project Alpha)"
@@ -174,7 +174,7 @@ const Home = () => {
             }}
           />
           {formError && <p className="input-error">{formError}</p>}
-        </div>
+        
         <button type="submit" className="primary">
           Add Board
         </button>
